28e1576be431dcf1ac692aa28a7eace5c17149c7
pbp15 <gatopbp123@gmail.com>
2020-11-09 20:18:33 -0500

    Proyecto fase inicial 1

---
+++ app/Http/Controllers/RecetaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecetaController extends Controller
+{
+    public function hola(){
+        return  view('nosotros');
+    }
+}
+
+
---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,22 @@
+<h1>Desde nosotros.blade.php</h1>
+
+{{1+ 1}}
+</br> 
+@php  
+
+    $variablr = 20
+
+ 
+
+
+@endphp
+
+@if ($variablr === 20 )
+
+    {{"Si es 20"}}
+
+</br>
+
+@endif
+
+{{$variablr}}
