6632bfa01d2c2bdb9a0a61e19046e400e00d47f9
pbp15 <gatopbp123@gmail.com>
2020-11-19 15:08:20 -0500

    Segmentación de redirecciones para las vistas del sistemas - arreglado las rutas click

--- resources/views/plantilla/sidebar.blade.php
+++ resources/views/plantilla/sidebar.blade.php
@@ -38,7 +38,7 @@
             <li class="nav-item nav-dropdown">
                 <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-basket"></i> Gestión de trámites</a>
                 <ul class="nav-dropdown-items">
-                    <li @click="menu=4" class="nav-item">
+                    <li @click="menu=5" class="nav-item">
                         <a class="nav-link" href="#"><i class="icon-basket-loaded"></i> Trámites</a>
                     </li>
                 </ul>
@@ -47,7 +47,7 @@
             <li class="nav-item nav-dropdown">
                 <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-basket"></i> Total de trámites</a>
                 <ul class="nav-dropdown-items">
-                    <li @click="menu=4" class="nav-item">
+                    <li @click="menu=6" class="nav-item">
                         <a class="nav-link" href="#"><i class="icon-basket-loaded"></i> Lista deTrámites</a>
                     </li>
                 </ul>
@@ -56,24 +56,24 @@
             <li class="nav-item nav-dropdown">
                 <a class="nav-link nav-dropdown-toggle" href="#"><i class="icon-pie-chart"></i> Reportes</a>
                 <ul class="nav-dropdown-items">
-                    <li @click="menu=9" class="nav-item">
+                    <li @click="menu=7" class="nav-item">
                         <a class="nav-link" href="#"><i class="icon-chart"></i> Entre fechas</a>
                     </li>
-                    <li  @click="menu=10" class="nav-item">
+                    <li  @click="menu=8" class="nav-item">
                         <a class="nav-link" href="#"><i class="icon-chart"></i> Por día </a>
                     </li>
-                    <li  @click="menu=11" class="nav-item">
+                    <li  @click="menu=9" class="nav-item">
                         <a class="nav-link" href="#"><i class="icon-chart"></i> Por Mes</a>
                     </li>
-                    <li  @click="menu=11" class="nav-item">
+                    <li  @click="menu=10" class="nav-item">
                         <a class="nav-link" href="#"><i class="icon-chart"></i> Últimos 7 días</a>
                     </li>
                 </ul>
             </li>
-            <li @click="menu=12" class="nav-item">
+            <li @click="menu=11" class="nav-item">
                 <a class="nav-link" href="#"><i class="icon-book-open"></i> Ayuda <span class="badge badge-danger">PDF</span></a>
             </li>
-            <li @click="menu=13" class="nav-item">
+            <li @click="menu=12" class="nav-item">
                 <a class="nav-link" href="#"><i class="icon-info"></i> Acerca de...<span class="badge badge-info">IT</span></a>
             </li>
         </ul>
