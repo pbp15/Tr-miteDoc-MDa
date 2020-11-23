fb0c01792bdc599e711964044f849b15d4456ad2
pbp15 <gatopbp123@gmail.com>
2020-11-22 21:22:07 -0500

    FUSION DE LAS RAMAS prueba1z(jane) y prueba2x(piero)

--- public/js/appdocumt.js
+++ public/js/appdocumt.js
@@ -53981,7 +53981,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\Tr-miteDoc-MDa\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Proyectos-Piero\Laravel-Gestiondoc\gestiondoc\resources\js\app.js */"./resources/js/app.js");
 
 
 /***/ })
